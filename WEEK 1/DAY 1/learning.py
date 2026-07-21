2a9ae493c689234cdfdab594d3b5f50255721c22
unknown <work.haniakhan@gmail.com>
2026-07-21 08:58:22 -0700

    [ADD] learning file

--- WEEK 1/DAY 1/learning.py
+++ WEEK 1/DAY 1/learning.py
@@ -1,7 +1,20 @@
 n = int(input("Enter the number:"))
+# Check wether a number is positive , negative or zero
 if n > 0:
     print("Positive Number")
 elif n < 0:
     print("Negative nember")
 else:
     print ("Zero")
+
+#Assign values to three variables
+a = 10 
+b = 20
+c = 15
+#Compare three numbers to finf the greatest
+if a > b and a > c :
+    print ("a is the greatest number ")
+elif b > a and b > c :    
+    print ("b is the greatest number ")
+else :
+    print("c is the greatest number ")
